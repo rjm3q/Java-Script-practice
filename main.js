b6e9598940c6bc028c0b6ccec123d1cd139efb5b
Robert <robert.mabry86@gmail.com>
2023-01-13 23:09:52 -0600

    its the final count down

--- main.js
+++ main.js
@@ -12,18 +12,14 @@ function greeting(language) {
   case "Spanish":
     greeting ="Hola!";
   default ="Hello";
-need to pass thru 
 return greeting
 }
 
 function isTenOrFive(num){
   // an if/else statement should do the trick
-if(num === 10 or 5){
-  return true;
-} else{
-  return false
-}
-}
+  // but the shorthand version is better 
+return(num === 10 || num=== 5)?true:false;
+
 
 function isInRange(num){
 if (num >= 20 or =< 50){
@@ -34,11 +30,20 @@ if (num >= 20 or =< 50){
 }
 
 function isInterger(num){
-
+  // i dont get how this is working, copied from the web 
+  return (num === parseInt(num, 10)) ? true : false;
 }
 
 function fizzBuzz(num){
-
+let num=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for (i in num){
+  if(num%i===0){
+    return false
+  }
+if (num===0||num===1){
+  return false
+}
+}
 }
 
 function isPrime(num){
