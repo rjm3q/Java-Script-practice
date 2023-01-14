e7f8e96b18c60d711c5b1a0c5bd73dbd1d36c47d
Robert <robert.mabry86@gmail.com>
2023-01-13 20:36:42 -0600

    JS practice begins...again

---
+++ main.js
@@ -0,0 +1,28 @@
+function getBiggest(x,y){
+  // Math.max finds the largest int
+  return Math.max(x,y)
+}
+
+function greeting(language) {
+  case "German":
+    greeting = "guten Tag!";
+  case "Mandarin":
+    greeting ="Ni Hao";
+  case "Spanish":
+    greeting ="Hola!";
+  default ="Hello";
+
+return greeting
+}
+
+function isTenOrFive(num){
+
+}
+
+function isInRange(num){
+
+}
+
+function isInterger(num){
+
+}
